07268010348c0874f97db85f77755113aa196bbb
stricklin <rubin.stricklin@gmail.com>
2017-06-08 12:59:37 -0700

    changed make_plots

--- make_plots.py
+++ make_plots.py
@@ -1,20 +1,23 @@
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 
+def make_plot(name):
+    rewards = np.load(name)
+    name = name.rstrip(".npy")
+    plt.title(name + " rewards")
+    plt.plot(list(range(1, len(rewards) * 100 + 1, 100)), rewards, 'blue', label="Training data")
+    plt.legend(loc='lower right')
+    plt.ylabel("Total rewards")
+    plt.xlabel("Episodes")
+    plt.savefig(name + ".png")
+
 if __name__ == "__main__":
-    training_rewards = np.load("training_rewards.npy")
-    testing_rewards = np.load("testing_rewards.npy")
-    plt.title("Training rewards")
-    plt.plot(list(range(1, len(training_rewards) * 100 + 1, 100)), training_rewards, 'blue', label="Training data")
-    plt.legend(loc='lower right')
-    plt.ylabel("Total rewards")
-    plt.xlabel("Episodes")
-    plt.show()
-    plt.title("Testing rewards")
-    plt.plot(list(range(1, len(testing_rewards) * 100 + 1, 100)), testing_rewards, 'red', label="Testing data")
-    plt.legend(loc='lower right')
-    plt.ylabel("Total rewards")
-    plt.xlabel("Episodes")
-    plt.show()
-
-
+    files = os.listdir()
+    files.sort()
+    plots = []
+    for file in files:
+        if file.endswith(".npy"):
+            plots.append(file)
+    for plot in plots:
+        make_plot(plot)
